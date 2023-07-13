07edd8802e13171655182cf32c9f23f836c5ef2d
Andrew Dickens <andrewcodes404@gmail.com>
2023-07-13 10:02:02 +0100

    prima

---
+++ wp-content/plugins/create-block-theme/build/plugin-sidebar.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-site', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives'), 'version' => '7feab5e833475d6f6912');
